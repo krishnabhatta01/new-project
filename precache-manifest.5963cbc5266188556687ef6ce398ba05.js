bd0485a416d8399447f880796ade67dc15fada1e
krishnabhatta01 <bhattakrishna111@gmail.com>
2020-08-12 18:32:16 +0545

    Updates

---
+++ precache-manifest.5963cbc5266188556687ef6ce398ba05.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff97cb1c3ef00021a645834206893926",
+    "url": "/new-project/index.html"
+  },
+  {
+    "revision": "f4e67a77fbc22df498c5",
+    "url": "/new-project/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "84ed59d2e41a20bd4bdd",
+    "url": "/new-project/static/css/main.8a0cc1d3.chunk.css"
+  },
+  {
+    "revision": "f4e67a77fbc22df498c5",
+    "url": "/new-project/static/js/2.b6a122a1.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/new-project/static/js/2.b6a122a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84ed59d2e41a20bd4bdd",
+    "url": "/new-project/static/js/main.73076171.chunk.js"
+  },
+  {
+    "revision": "96a07afa24019b897795",
+    "url": "/new-project/static/js/runtime-main.56f95786.js"
+  },
+  {
+    "revision": "c3bc31874ef5b5d849aa16d7ecc6c593",
+    "url": "/new-project/static/media/about.c3bc3187.jpg"
+  },
+  {
+    "revision": "6b00952718cbd46612be564b086396b0",
+    "url": "/new-project/static/media/com.6b009527.jpg"
+  },
+  {
+    "revision": "943dde18a6609b7f5e20e7df944122e7",
+    "url": "/new-project/static/media/graphics.943dde18.jpg"
+  },
+  {
+    "revision": "f4ababeb5200889c25bdfcd7e9f7324b",
+    "url": "/new-project/static/media/krishna.f4ababeb.jpg"
+  },
+  {
+    "revision": "96880cc96b3f6cc30181e15796664b9d",
+    "url": "/new-project/static/media/problems.96880cc9.jpg"
+  },
+  {
+    "revision": "673d33b468080add33d7dd84a889ce5a",
+    "url": "/new-project/static/media/security.673d33b4.jpg"
+  },
+  {
+    "revision": "46d636e58622c15a36310f914acfee1a",
+    "url": "/new-project/static/media/software.46d636e5.jpg"
+  },
+  {
+    "revision": "60f387e0c9cc8dd7cd006be80f7403cf",
+    "url": "/new-project/static/media/webdesign.60f387e0.jpg"
+  },
+  {
+    "revision": "bb5f13a154cc319f003753387eeaf4c7",
+    "url": "/new-project/static/media/webdevelopment.bb5f13a1.jpg"
+  }
+]);
